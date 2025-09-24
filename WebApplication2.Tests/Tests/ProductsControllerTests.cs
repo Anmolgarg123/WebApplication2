b90301e6561bc278066360711305d60383784463
Anmolgarg123 <55012991+Anmolgarg123@users.noreply.github.com>
2025-09-24 06:43:47 +0530

    Update ProductsControllerTests.cs

--- WebApplication2.Tests/Tests/ProductsControllerTests.cs
+++ WebApplication2.Tests/Tests/ProductsControllerTests.cs
@@ -2,6 +2,7 @@ using WebApplication2.Controllers;
 using WebApplication2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
+using System;
 
 namespace WebApplication2.Tests
 {
@@ -19,12 +20,24 @@ namespace WebApplication2.Tests
                 Price = 99.99m
             };
 
+            Console.WriteLine($"[TEST] Input Product: Id={product.Id}, Name={product.Name}, Price={product.Price}");
+
             // Act
             ActionResult<Product> actionResult = controller.Create(product);
             Assert.NotNull(actionResult.Result);
 
             var createdResult = actionResult.Result as CreatedAtActionResult;
             Assert.NotNull(createdResult);
+
+            // Console output for the result
+            if (createdResult?.Value is Product createdProduct)
+            {
+                Console.WriteLine($"[TEST] Created Product: Id={createdProduct.Id}, Name={createdProduct.Name}, Price={createdProduct.Price}");
+            }
+            else
+            {
+                Console.WriteLine("[TEST] Created result value is null or not a Product");
+            }
 
             // Assert
             Assert.Equal(201, createdResult!.StatusCode);
